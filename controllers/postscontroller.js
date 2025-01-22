76c6008b2999d13a42d8268e00ab84603fee28c7
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-01-22 14:56:21 +0100

    feat: milestone 2 completed

--- controllers/postscontroller.js
+++ controllers/postscontroller.js
@@ -2,6 +2,19 @@ const connection = require("../data/database");
 
 //index --> GET
 const index = (req, res) => {
+ const sql = "SELECT * FROM `posts`";
+ connection.query(sql, (err, results) => {
+    if(err) {
+        return res.status(500).json({
+            message: "Errore interno del server"
+        });
+    } else {
+        return res.status(200).json({
+            status: "success",
+            data: results
+        })
+    }
+ })
     
 }
 
